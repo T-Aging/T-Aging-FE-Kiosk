93008a506d67737c6cfef944c268641ac75c8e7f
sunny <lesunhe@gmail.com>
2025-11-18 18:07:20 +0900

    feat(SplashScreen): 스플래시 화면 ui 완성 및 공통 레이아웃 헤더 포함

--- T-AGING-FE-KIOSK/src/components/layout/KioskLayout.tsx
+++ T-AGING-FE-KIOSK/src/components/layout/KioskLayout.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const KioskLayout = () => {
+  const [title, setTitle] = useState("마실이 키오스크");
   return (
-    <div className="flex justify-center h-dvh w-dvw ">
+    <div className="flex items-center justify-center h-dvh w-dvw">
       <div className="flex h-full w-full max-w-[1600px]  flex-col bg-(--bg-primary)">
+        <header className="w-full py-6 text-center text-[5vw] font-bold text-(--text-primary) ">
+          {title}
+        </header>
         <main className="w-full h-full">
-          <Outlet />
+          <Outlet context={{ setTitle }} />
         </main>
       </div>
     </div>
--- T-AGING-FE-KIOSK/src/pages/SplashScreen.tsx
+++ T-AGING-FE-KIOSK/src/pages/SplashScreen.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 const SplashScreen = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const { setTitle } = useOutletContext<{ setTitle: (v: string) => void }>();
+
+  useEffect(() => {
+    setTitle("마실이 키오스크");
+  }, [setTitle]);
+
+  const handleStart = () => {
+    navigate("/membership");
+  };
+
+  return (
+    <div className="relative flex h-full w-full flex-col items-center justify-center bg-(--bg-primary)">
+      <img
+        src="src/assets/images/masil.png"
+        alt="masil-logo"
+        className="mb-6 h-[50vw] w-[50vw]"
+      />
+
+      <h1 className="text-[6rem] font-semibold text-(--text-primary) mb-2">
+        환영합니다
+      </h1>
+
+      <p className="text-[3rem] text-(--text-secondary) mb-10 text-center leading-tight">
+        주문을 시작하려면 <br /> 화면을 터치하세요
+      </p>
+
+      <button
+        onClick={handleStart}
+        className="bg-(--color-primary) text-(--text-inverse) text-[3rem] font-medium
+        px-12 py-4 rounded-xl shadow-md active:scale-95 transition"
+      >
+        주문 시작하기
+      </button>
+
+      <footer className="h-[12vh] flex items-center justify-center text-[3vw] text-(--text-tertiary)">
+        🔊 음성 안내 중입니다
+      </footer>
+    </div>
+  );
 };
 
 export default SplashScreen;
