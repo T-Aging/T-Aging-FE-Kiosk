7126370e641f481adfab808192a56c2138dd6e0b
sunny <lesunhe@gmail.com>
2025-11-29 03:33:42 +0900

    feat: 키오스크 헤더 고정, main부분만 스크롤 구조로 변경

--- T-AGING-FE-KIOSK/src/components/layout/KioskLayout.tsx
+++ T-AGING-FE-KIOSK/src/components/layout/KioskLayout.tsx
@@ -3,13 +3,17 @@ import { Outlet } from "react-router-dom";
 
 const KioskLayout = () => {
   const [title, setTitle] = useState("마실이 키오스크");
+
   return (
-    <div className="flex items-center justify-center h-dvh w-dvw">
-      <div className="flex h-full w-full max-w-[1600px]  flex-col bg-(--bg-primary)">
-        <header className="w-full py-6 text-center text-[5vw] font-bold text-(--text-primary) ">
+    <div className="flex h-dvh w-dvw items-center justify-center overflow-hidden">
+      <div className="flex h-full w-full flex-col bg-(--bg-primary)">
+        {/* HEADER */}
+        <header className="w-full py-6 text-center text-[5vw] font-bold text-(--text-primary)">
           {title}
         </header>
-        <main className="w-full h-full">
+
+        {/* MAIN */}
+        <main className="w-full flex-1 overflow-hidden">
           <Outlet context={{ setTitle }} />
         </main>
       </div>
